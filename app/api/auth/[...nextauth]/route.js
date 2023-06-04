e01cc9a6be82d2f443019dbd999dde5ae548ad49
islam-ahsanul <ahsanulislam.info@gmail.com>
2023-06-04 23:32:41 +0600

    auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
 import { signIn } from 'next-auth/react';
+import { connectToDB } from '@utils/database';
+import User from '@models/user';
 
 console.log({
   clientId: process.env.GOOGLE_ID,
@@ -14,9 +16,40 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
 
-  async signIn({ profile }) {},
+    session.user.id = sessionUser._id.toString();
+
+    return session;
+  },
+
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+
+      // check if a user is already exists
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+
+      // if not, create a new user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(' ', '').toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 });
 
 export { handler as GET, handler as POST };
