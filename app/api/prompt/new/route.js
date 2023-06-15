837511d0326288d399d6f0c583ed38b2b27f6301
islam-ahsanul <ahsanulislam.info@gmail.com>
2023-06-15 07:55:29 +0600

    get posts from server for card

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -2,13 +2,13 @@ import { connectToDB } from '@utils/database';
 import Prompt from '@models/prompt';
 
 export const POST = async (request) => {
-  const { userId, propmt, tag } = await request.json();
+  const { userId, prompt, tag } = await request.json();
 
   try {
     await connectToDB();
     const newPrompt = new Prompt({
       creator: userId,
-      propmt,
+      prompt,
       tag,
     });
 
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,57 @@
-import React from 'react';
+'use client';
+import { useState, useEffect } from 'react';
+import PromptCard from './PromptCard';
 
+// Card List component
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
+
+// Feed component
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setsearchText] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+
+      setPosts(data);
+      console.log(data);
+    };
+
+    // console.log(data);
+    fetchPosts();
+  }, []);
+
+  const handleSearchChange = (e) => {};
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          className="search_input peer"
+          type="text"
+          placeholder="Search for a tag or user"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+        />
+      </form>
+
+      <PromptCardList data={posts} handleClick={() => {}} />
+    </section>
+  );
 };
 
 export default Feed;
